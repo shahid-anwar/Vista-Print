1e211447d2ba5acea31af49f6c5b8e7f0e788d4d
shahid-anwar <shahidanwar3548@gmail.com>
2024-03-18 20:42:11 +0530

    Header

---
+++ vista/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import MainHeader from "./Component/Header/Header";
+
+function App() {
+  return (
+    <div>
+      <MainHeader />
+    </div>
+  );
+}
+
+export default App;
