b6a3c9152e6eaf3d45dc0bae259071ab9c3c7f9d
shahid-anwar <shahidanwar3548@gmail.com>
2024-03-19 01:32:24 +0530

    Nav Completed

--- vista/src/App.jsx
+++ vista/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import MainHeader from "./Component/Header/Header";
+import Nav from "./Component/Header/Nav";
 
 function App() {
   return (
     <div>
       <MainHeader />
+      <Nav />
     </div>
   );
 }
---
+++ vista/src/Component/Header/Nav.jsx
@@ -0,0 +1,43 @@
+import "./styles.css";
+const Nav = () => {
+  return (
+    <div>
+      {/* <nav className="header-nav">
+        <ul>
+          <li className="li-all">
+            <span>View All</span>
+          </li>
+          <li className="li-all"> Visiting Cards</li>
+          <li className="li-all">Stationary</li>
+          <li className="li-all">Personalised Pens</li>
+          <li className="li-all">Stamps and Inks</li>
+          <li className="li-all">Signs, Posters and Marketing Materials</li>
+          <li className="li-all">Labels,Stickers & Packaging</li>
+          <li className="li-all">Clothing Caps & Bags</li>
+          <li className="li-all">Mugs, Albums & Gifts</li>
+          <li className="li-all">Drinkware</li>
+          <li className="li-all">New Arrivals</li>
+          <li className="li-all">Custom Polo T-shirts</li>
+        </ul>
+      </nav> */}
+      <nav className="header-nav">
+        <ul>
+          <li className="li-all">View All</li>
+          <li className="li-all">Visiting Cards</li>
+          <li className="li-all">Stationery</li>
+          <li className="li-all">Personalized Pens</li>
+          <li className="li-all">Stamps & Inks</li>
+          <li className="li-all">Posters & Marketing</li>
+          <li className="li-all">Labels & Stickers</li>
+          <li className="li-all">Clothing & Bags</li>
+          <li className="li-all">Mugs & Gifts</li>
+          <li className="li-all">Drinkware</li>
+          {/* <li className="li-all">New Arrivals</li> */}
+          <li className="li-all">Custom Polo T-Shirts</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
